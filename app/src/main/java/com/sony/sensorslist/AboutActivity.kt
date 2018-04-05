3cf3ed387bb6fad18037f81f4cf214b848401df7
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-04-05 17:17:30 +0200

    build SpannableString using android-ktx

--- app/src/main/java/com/sony/sensorslist/AboutActivity.kt
+++ app/src/main/java/com/sony/sensorslist/AboutActivity.kt
@@ -1,12 +1,10 @@
 package com.sony.sensorslist
 
-import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.text.method.ScrollingMovementMethod
-import android.text.style.StyleSpan
+import androidx.core.text.bold
 import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
@@ -23,15 +21,12 @@ class AboutActivity : AppCompatActivity() {
                     it.readText()
                 }
 
-        val appNameStr = resources.getString(R.string.app_name)
-        val licenseStr = resources.getString(R.string.license_title)
-
         aboutText.text = SpannableStringBuilder()
-                .append(appNameStr, StyleSpan(Typeface.BOLD), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                .bold { append(resources.getString(R.string.app_name)) }
                 .append("\n")
                 .append(BuildConfig.VERSION_NAME)
                 .append("\n\n")
-                .append(licenseStr, StyleSpan(Typeface.BOLD), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                .bold { append(resources.getString(R.string.license_title)) }
                 .append("\n")
                 .append(licenseText)
                 .append("\n")
