154b8c2d16164726fea3c8f9a117f975cdacce08
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-04-09 15:35:49 +0200

    minor formatting fixes

--- app/src/main/java/com/sony/sensorslist/MainActivity.kt
+++ app/src/main/java/com/sony/sensorslist/MainActivity.kt
@@ -51,8 +51,11 @@ class MainActivity : AppCompatActivity(),
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(this,
+                drawer_layout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
